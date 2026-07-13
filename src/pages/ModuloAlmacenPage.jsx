8f59f778a575bf2f36b1faf4a2df8b82e822b551
Farit Espinoza <faritespinoza@gmail.com>
2026-07-12 20:34:56 -0500

    refactor: retirar frontend obsoleto y dependencias sin uso

--- src/pages/ModuloAlmacenPage.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/ModuloAlmacenPage.jsx
-import React from "react";
-import LayoutAlmacen from "../components/LayoutAlmacen";
-
-const ModuloAlmacenPage = () => {
-  return <LayoutAlmacen />;
-};
-
-export default ModuloAlmacenPage;
--- src/pages/ProductosAlmacenPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProductosAlmacenPage = () => {
-  return <div>ProductosAlmacenPage</div>;
-};
-
-export default ProductosAlmacenPage;
